7d2e10f1bfb9887a7a40bc3cb0d8254963aca4fd
sdiahid609 <xsdiahid609@ieshnosmachado.org>
2023-11-08 09:55:15 +0100

    Update views.py

--- task/views.py
+++ task/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Task
-from .forms import TaskForm
+from .forms import TaskForm #se crea como clase TaskForm para poder importarlo
 from django.shortcuts import redirect
 
 
@@ -9,12 +9,12 @@ def task_list(request):
     return render(request, 'task/task_list.html', {'tasks' : tasks})
     
 def task_new(request):
-    if request.method == "POST":
-        form = TaskForm(request.POST)
-        if form.is_valid():
+    if request.method == "POST": #Dos situaciones, si el form esta vacio y cuando se envia.
+        form = TaskForm(request.POST) #El form recibe el request.post
+        if form.is_valid(): 
             task = form.save(commit=False)
             task.save()
             return redirect('task_list')
     else:
-        form = TaskForm()
+        form = TaskForm() #Esta creando un formulario vacío
     return render(request, 'task/task_edit.html', {'form': form})
